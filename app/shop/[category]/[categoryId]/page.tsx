e9f738c29d304fcccb6f6f0bac38a6a8900f41c1
P4rley <adwihersa@gmail.com>
2023-07-13 10:27:11 +0700

    created wishlist button

--- app/shop/[category]/[categoryId]/page.tsx
+++ app/shop/[category]/[categoryId]/page.tsx
@@ -1,6 +1,15 @@
 "use client";
 
 import Rating from "@/components/Rating";
+import Image from "next/image";
+import img from "../../../heroImg.jpg";
+import {
+  AiOutlineArrowUp,
+  AiOutlineArrowDown,
+  AiOutlineHeart,
+  AiFillHeart,
+} from "react-icons/ai";
+import { BsArrowRightCircle, BsArrowLeftCircle } from "react-icons/bs";
 import { useState } from "react";
 
 type Props = {
@@ -9,11 +18,98 @@ type Props = {
   };
 };
 const DetailPage = ({ params: { categoryId } }: Props) => {
-  const [rating, setRating] = useState<number>(0);
+  const [showSize, setShowSize] = useState<boolean>(false);
+  const [size, setSize] = useState<string>("king");
+  const [quantity, setQuantity] = useState<number>(0);
+  const [wishlist, setWishlist] = useState<boolean>(false);
+
   return (
-    <div>
+    <div className="flex flex-col gap-4 my-5">
       <div className="">
-        <Rating />
+        <Image src={img} alt="Product Image" className="w-full h-[250px]" />
+      </div>
+
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-3">
+          <div className="flex gap-1">
+            <Rating />
+          </div>
+          <span>Item No.1234567</span>
+        </div>
+
+        <div
+          className="border border-[#222] p-2 w-full transition-all ease-in-out duration-300"
+          onClick={() => setShowSize(!showSize)}
+        >
+          <div className="flex justify-between items-center">
+            <p>Size</p>
+            <div className="flex gap-2 items-center justify-center">
+              <p>King</p>
+              <button>
+                {showSize ? <AiOutlineArrowUp /> : <AiOutlineArrowDown />}
+              </button>
+            </div>
+          </div>
+
+          {showSize ? (
+            <div className="p-4 flex justify-between gap-4">
+              <button
+                onClick={() => setSize("queen")}
+                className={`border border-[#222] p-1 w-[150px] transition-all ease-in-out duration-300 ${
+                  size === "queen" ? "bg-[#222] text-white" : "text-[#222]"
+                }`}
+              >
+                Queen
+              </button>
+              <button
+                onClick={() => setSize("king")}
+                className={`border border-[#222] p-1 w-[150px] transition-all ease-in-out duration-300 ${
+                  size === "king" ? "bg-[#222] text-white" : "text-[#222]"
+                }`}
+              >
+                King
+              </button>
+            </div>
+          ) : null}
+        </div>
+
+        <div className="flex gap-5">
+          <p className="text-2xl font-medium">Quantity: </p>
+
+          <div className="flex gap-3 items-center">
+            <button
+              onClick={() => setQuantity((prev) => (prev -= 1))}
+              disabled={quantity < 1 ? true : false}
+              className="disabled:text-gray-400 "
+            >
+              <BsArrowLeftCircle fontWeight={900} size={24} />
+            </button>
+
+            <span className="text-2xl">
+              {quantity < 0 ? `00` : quantity < 10 ? `0${quantity}` : quantity}
+            </span>
+
+            <button
+              onClick={() => setQuantity((prev) => (prev += 1))}
+              className=""
+            >
+              <BsArrowRightCircle fontWeight={900} size={24} />
+            </button>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <button className="p-2 bg-[#222] w-full text-white">
+            Add to cart
+          </button>
+          <button onClick={() => setWishlist(!wishlist)}>
+            {wishlist ? (
+              <AiFillHeart color="red" size={24} />
+            ) : (
+              <AiOutlineHeart size={24} />
+            )}
+          </button>
+        </div>
       </div>
     </div>
   );
--- components/Rating.tsx
+++ components/Rating.tsx
@@ -6,23 +6,26 @@ type Props = {};
 const Rating = ({}: Props) => {
   const [rating, setRating] = useState<number>(0);
   return (
-    <div>
-      {[...Array(5)].map((star, index) => {
-        index += 1;
-        return (
-          <button
-            type="button"
-            key={index}
-            className={index <= rating ? "text-yellow-500" : "text-gray-300"}
-            onClick={() => setRating(index)}
-            onMouseOver={() => setRating(index)}
-          >
-            <span className="border-none bg-transparent outline-none">
-              &#9733;
-            </span>
-          </button>
-        );
-      })}
+    <div className="flex gap-1">
+      <div>
+        {[...Array(5)].map((star, index) => {
+          index += 1;
+          return (
+            <button
+              type="button"
+              key={index}
+              className={index <= rating ? "text-yellow-500" : "text-gray-300"}
+              onClick={() => setRating(index)}
+              onMouseOver={() => setRating(index)}
+            >
+              <span className="border-none bg-transparent outline-none">
+                &#9733;
+              </span>
+            </button>
+          );
+        })}
+      </div>
+      <p>({rating})</p>
     </div>
   );
 };
