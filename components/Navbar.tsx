81eba8bfb4a693075e19f6376db6b9fdfa728593
P4rley <adwihersa@gmail.com>
2023-07-14 10:53:33 +0700

    Created search input

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,22 +5,38 @@ import { usePathname } from "next/navigation";
 import { FaSearch, FaChair } from "react-icons/fa";
 import { HiMenuAlt1 } from "react-icons/hi";
 import { MdClose } from "react-icons/md";
+import { IoMdClose } from "react-icons/io";
+import { useRouter } from "next/navigation";
 import "@/styles/Navbar.css";
-import { useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 
 type Props = {};
 const Navbar = (props: Props) => {
   const pathname = usePathname();
   const [open, setOpen] = useState<boolean>(false);
+  const [showInput, setShowInput] = useState<boolean>(false);
+  const [search, setSearch] = useState<string>("");
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const encodedSearchQuery = encodeURI(search);
+
+    if (search) {
+      router.push(`${pathname}?search=${encodedSearchQuery}`);
+    } else {
+      router.push(`${pathname}`);
+    }
+  }, [search, pathname, router]);
 
   return (
     <div className="px-[10%] md:px-[4rem] lg:px-[8rem]">
       <div className="flex justify-between items-center border-b-[2px] border-[#222] py-[1rem] lg:py-[2rem]">
-        <div className="">
+        <div className="flex-1 ">
           <h1 className="font-bold text-[24px]">Furniday</h1>
         </div>
 
-        <ul className=" gap-3 hidden md:flex">
+        <ul className=" gap-3 hidden md:flex flex-1 md:justify-center">
           <li
             className={`font-semibold text-base relative ${
               pathname === `/` ? "text-[#222] active-link" : "text-[#717171]"
@@ -45,8 +61,42 @@ const Navbar = (props: Props) => {
           })}
         </ul>
 
-        <div className="gap-3 items-center hidden md:flex">
-          <FaSearch fontSize={18} color={"#222"} className="cursor-pointer" />
+        <div className="gap-3 items-center justify-end hidden md:flex md:flex-1">
+          <label
+            htmlFor="search"
+            className={`flex flex-row items-center border-2 border-[#222] justify-center gap-2 px-2 overflow-hidden relative ${
+              showInput ? "w-[220px] h-[40px]" : "h-[40px] w-[40px]"
+            } rounded-full transition-all ease-in-out duration-300`}
+          >
+            <input
+              id="search"
+              type="text"
+              placeholder="Search"
+              className={`outline-none border-none pr-4 ${
+                showInput ? "block" : "hidden"
+              }`}
+              onChange={(e) => setSearch(e.target.value)}
+              value={search}
+            />
+            {showInput ? (
+              <IoMdClose
+                fontSize={20}
+                color={"#222"}
+                className="cursor-pointer absolute right-[10px]"
+                onClick={() => {
+                  setSearch("");
+                  setShowInput(false);
+                }}
+              />
+            ) : (
+              <FaSearch
+                fontSize={14}
+                color={"#222"}
+                className="cursor-pointer absolute right-[10px]"
+                onClick={() => setShowInput(true)}
+              />
+            )}
+          </label>
 
           <div className="w-[2px] rounded-lg h-[30px] bg-[#222]" />
 
